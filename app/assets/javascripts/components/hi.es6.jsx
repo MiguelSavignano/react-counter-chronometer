e82fdb1b198e8f9dba81b7980d7ca4ce991af1ed
Miguel <migue.masx@gmail.com>
2016-07-31 18:47:33 +0200

    fix clear time out at time

--- app/assets/javascripts/components/hi.es6.jsx
+++ app/assets/javascripts/components/hi.es6.jsx
@@ -11,8 +11,11 @@ class Hi extends React.Component {
   }
   timerStart(){
     var interval_fnc = setInterval((time) => {
-      this.setState({ timer: this.state.timer - 0.1 })
-      if(this.state.timer <= 0) clearTimeout(this.state.interval_fnc);
+      if(this.state.timer <= 0){
+        clearTimeout(this.state.interval_fnc);
+      }else{
+        this.setState({ timer: this.state.timer - 0.1 })
+      } 
     }, 100);
     this.setState({ interval_fnc: interval_fnc })
   }
