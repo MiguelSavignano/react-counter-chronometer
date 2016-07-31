a2bb46d52334dc098ef71abced475aebe235529a
Miguel <migue.masx@gmail.com>
2016-07-31 18:15:17 +0200

    timmer ok

--- app/assets/javascripts/components/hi.es6.jsx
+++ app/assets/javascripts/components/hi.es6.jsx
@@ -2,22 +2,21 @@ class Hi extends React.Component {
   constructor(props) {
     super(props)
     _.bindAll(this, 'timerStart')
-    this.props.MAX_TIMER = 30
     this.state = {
       count: 0,
-      timer: 0
+      timer: 10
     }
   }
   onCountUp(){
     if( this.state.count == 0 ) this.timerStart()
-    if(this.state.timer < this.props.MAX_TIMER){
+    if(this.state.timer >= 0){
       this.setState({ count: this.state.count + 1 })
     }
   }
   timerStart(){
     var interval_fnc = setInterval((time) => {
-      if(this.state.timer > this.props.MAX_TIMER) clearTimeout(this.state.interval_fnc);
-      this.setState({ timer: this.state.timer + 0.1 })
+      if(this.state.timer <= 0) clearTimeout(this.state.interval_fnc);
+      this.setState({ timer: this.state.timer - 0.1 })
     }, 100);
     this.setState({ interval_fnc: interval_fnc })
   }
