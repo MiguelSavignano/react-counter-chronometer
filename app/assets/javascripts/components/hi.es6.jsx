0f868d732de355c7cdce4c559b12e79035f6b42e
Miguel <migue.masx@gmail.com>
2016-07-31 19:54:47 +0200

    max count

--- app/assets/javascripts/components/hi.es6.jsx
+++ app/assets/javascripts/components/hi.es6.jsx
@@ -2,7 +2,7 @@ class Hi extends React.Component {
   constructor(props) {
     super(props)
     _.bindAll(this, ['timerStart', 'restart'])
-    this.state = { count: 0, timer: 10 }
+    this.state = { count: 0, timer: 10, maxCout: 0 }
   }
   onCountUp(){
     if( this.state.count == 0 ) this.timerStart()
@@ -12,10 +12,11 @@ class Hi extends React.Component {
   timerStart(){
     var interval_fnc = setInterval((time) => {
       if(this.state.timer <= 0){
+        this.setState({ maxCout: this.state.count })
         clearTimeout(this.state.interval_fnc);
       }else{
         this.setState({ timer: this.state.timer - 0.1 })
-      } 
+      }
     }, 100);
     this.setState({ interval_fnc: interval_fnc })
   }
@@ -24,11 +25,18 @@ class Hi extends React.Component {
     this.setState({ count: 0,  timer: 10 })
   }
   render() {
-    const {count, timer} = this.state
+    const {count, timer, maxCout} = this.state
     return (
       <div>
-        timer  : {timer} <br/>
-        clicks: {count} <br/>
+        { count == 0 ?
+          <h4>Cuantas veces puedes hacer click en el boton en 10 segundos?</h4>
+          :
+          <h2>{timer}</h2>
+        }
+        { (maxCout != 0) && <h4>clicks maximos: {maxCout}</h4> }
+        <br/>
+        <h4>clicks: {count}</h4> <br/>
+      <br/>
       <button className="btn btn-info btn-lg"
           onClick={this.onCountUp.bind(this)}>
           click
